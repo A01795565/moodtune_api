b8795201bfdbd116e5983a404c86030a03f5d574
Ramiro Gonzalez Carrillo <a01795565@tec.mx>
2025-10-24 20:30:45 -0600

    Ajustes de la base de datos

--- app/src/config.py
+++ app/src/config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 class Config:
     # DB
     load_dotenv()
-    DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-    DB_PORT = int(os.getenv("DB_PORT", "3306"))
-    DB_USER = os.getenv("DB_USER", "user")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-    DB_NAME = os.getenv("DB_NAME", "database")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = int(os.getenv("DB_PORT"))
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
     DB_CHARSET = "utf8mb4"
     DB_COLLATION = "utf8mb4_0900_ai_ci"
     DB_POOL_NAME = os.getenv("DB_POOL_NAME", "moodtune_pool")
@@ -16,5 +16,5 @@ class Config:
 
     DEBUG = os.getenv("DEBUG", "true").lower() == "true"
 
-    # Bandera por si decides impedir borrar logs de auditoría en producción
+    # Bandera para impedir borrar logs de auditoría en producción
     ALLOW_AUDIT_DELETE = os.getenv("ALLOW_AUDIT_DELETE", "false").lower() == "true"
